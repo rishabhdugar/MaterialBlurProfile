e47b8b5c90f1ce772a4bd348e97a2de3cac1bf9c
Rishabh Dugar <rishabhdugar@Rishabhs-MacBook-Pro.local>
2019-02-03 21:05:28 +0530

    First Commit-Basic Activity

---
+++ app/src/main/java/com/example/rishabhdugar/materialblurbackground/util/CustomLayout.java
@@ -0,0 +1,42 @@
+package com.example.rishabhdugar.materialblurbackground.util;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.Target;
+
+public class CustomLayout extends RelativeLayout implements Target {
+
+    public CustomLayout(Context context) {
+        super(context);
+    }
+
+    public CustomLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public CustomLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+    @Override
+    public void onBitmapLoaded(Bitmap bitmap, Picasso.LoadedFrom from) {
+        setBackground(new BitmapDrawable(getResources(), bitmap));
+    }
+
+    @Override
+    public void onBitmapFailed(Drawable errorDrawable) {
+        //Set your error drawable
+    }
+
+    @Override
+    public void onPrepareLoad(Drawable placeHolderDrawable) {
+        //Set your placeholder
+    }
+}
